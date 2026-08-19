d38cb15e7f3a537d321d57f69bfc4de063e5c9eb
patricksebastian-lothus <patricksebastian@agencialothus.com>
2026-08-19 16:22:35 -0300

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,7 +2,7 @@
   // Configuração pública do frontend.
   // Em produção com domínio próprio, altere este arquivo uma vez em vez de editar todos os HTMLs.
   const DEFAULT_API_BASE = "https://dialoga-backend-r1zp.onrender.com";
-  const DEFAULT_FRONTEND_BASE = "https://dialoga-frontend-wir.onrender.com";
+  const DEFAULT_FRONTEND_BASE = "https://dialoga-frontend-wird.onrender.com";
 
   global.API_BASE = global.API_BASE || DEFAULT_API_BASE;
   global.WF_PUBLIC_CONFIG = Object.assign({}, global.WF_PUBLIC_CONFIG || {}, {
